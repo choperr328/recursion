e2af97874d2b1df867140019dbdc9c2d9b3038ff
choperr328 <iliabaksanov777@yandex.ru8>
2024-06-05 22:50:28 +0200

    git add files

---
+++ akkerman/Program.cs
@@ -0,0 +1,24 @@
+﻿int akkerman(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    else if (m == 0)
+    {
+        return akkerman(n - 1, 1);
+    }
+    return akkerman(n - 1, akkerman(n, m - 1));
+
+}
+
+Console.WriteLine("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int x = akkerman(n, m);
+Console.WriteLine(x);
+
+
---
+++ search/Program.cs
@@ -0,0 +1,20 @@
+﻿void naturalOrder(int m, int n)
+{
+    if (m < n)
+    {
+        Console.Write($"{m} ");
+        naturalOrder(m + 1, n);
+        
+    }
+    else
+    {
+        if(m > n)
+        {
+            Console.Write($"{m} ");
+            naturalOrder(m-1,n);
+        }
+    }
+}
+
+
+naturalOrder(123,65);
