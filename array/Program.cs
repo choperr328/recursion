dbf3655775e5616332b235b82a88bff026a931ba
choperr328 <iliabaksanov777@yandex.ru8>
2024-06-05 22:56:31 +0200

    add files

---
+++ array/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 2, 5, 0, 10, 34 };
+void matreshka(int size, int[] arr)
+{
+
+    if (size < 1)
+    {
+        Console.WriteLine($"{arr[size]}");
+        return;
+    }
+    Console.Write($"{array[size]} ");
+    matreshka(size - 1, array);
+
+
+
+}
+matreshka(array.Length-1, array);
